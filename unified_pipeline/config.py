608049de83c9e7edd63fd9aaaa92dda19b53e66f
Nishit <nishitsuthar123@gmail.com>
2026-09-09 15:36:13 +0200

    refactor: remove unused RowF1Evaluator (superseded by TableF1Evaluator)

--- unified_pipeline/config.py
+++ unified_pipeline/config.py
@@ -9,7 +9,6 @@ from unified_pipeline.evidence.csv_full_evidence import CsvFullEvidenceBuilder  
 from unified_pipeline.evidence.rag_evidence import RagEvidenceBuilder            # Nishit adds
 # from unified_pipeline.evidence.visual_pdf_evidence import VisualPdfEvidenceBuilder  # Arpitha adds
 
-from unified_pipeline.evaluators.row_f1_evaluator import RowF1Evaluator
 from unified_pipeline.evaluators.table_f1_evaluator import TableF1Evaluator    # Krittika
 # from unified_pipeline.evaluators.exact_match_evaluator import ExactMatchEvaluator   # Satwik adds
 from unified_pipeline.evaluators.span_f1_evaluator import SpanF1Evaluator           # Nishit adds
@@ -24,7 +23,6 @@ EVIDENCE_BUILDERS = {
 }
 
 EVALUATORS = {
-    "row_f1":         RowF1Evaluator,
     "table_f1":       TableF1Evaluator,
     # "exact_match":    ExactMatchEvaluator,
     "span_f1":        SpanF1Evaluator,
